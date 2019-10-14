a327b39a1db55576db1d745569926d452906768c
Chong Jin Yi <chong601.13@gmail.com>
2019-10-14 19:22:50 +0800

    Add missing configurations required on config.py Extra patch to allow the app to pull ZFS configuration from config.py

--- config.py
+++ config.py
@@ -2,7 +2,14 @@ class Config(object):
     """
     Common configurations
     """
-
+    # Name of the ZFS pool to use
+    ZFS_POOL = 'zfs_pool_ubuntu'
+    # Name of the dataset
+    ZFS_DATASET = 'kvm-images'
+    # Mountpoint for the dataset in use
+    # TODO: add code to extract the mountpoint of the given pool and dataset
+    #       if the ZFS_MOUNTPOINT is empty
+    ZFS_MOUNTPOINT = '/kvm-images'
     DEBUG = True
 
 
